ebcb378dc601af89c9d165f3711e82a775f3102a
maryobrien3 <mobrien051408@gmail.com>
2025-04-01 17:17:57 -0400

    Refactor main app structure and enhance UI with buttons for login and account creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MainApp(),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,24 +11,51 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'STEM Project Planner',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
       home: Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min, // Ensures the column takes up minimal space
-              children: [
-                Icon(
-                  Icons.favorite,
-                  size: 200,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: Icon(
+                  Icons.edit_calendar, // will be changed to the logo once we make one
+                  color: Color.fromARGB(255, 58, 110, 222),
+                  size: 150,
                 ),
-                Text(
-                  'Variny Project Planner',
-                  style: TextStyle(fontSize: 30),
-                ),
-              ],
-            ),
+              ),
+              Text(
+                'Variny Project Planner',
+                style: TextStyle(fontSize: 30),
+              ),
+              SizedBox(height: 20), // Add spacing between the text and the row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add functionality for the first button here
+                    },
+                    child: Text('Login'),
+                  ),
+                  SizedBox(width: 20), // Add spacing between the buttons
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add functionality for the second button here
+                    },
+                    child: Text('Create Account'),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
-      );
-        }
-      }
+      ),
+    );
+  }
+}
