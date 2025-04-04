94af69d3e491050068f95928301ef58a1408a8bf
maryobrien3 <mobrien051408@gmail.com>
2025-04-04 12:51:02 -0400

    Enhance navigation by adding Login and Create Account pages with updated UI elements

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MainApp(),
+    const MaterialApp(
+      title: 'Navigation Basics', 
+      home: MainApp(),
+    ),
   );
 }
 
@@ -11,13 +14,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'STEM Project Planner',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: Scaffold(
-        body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        
+      ),  
+      body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -25,28 +26,37 @@ class MainApp extends StatelessWidget {
                 padding: const EdgeInsets.only(bottom: 20.0),
                 child: Icon(
                   Icons.edit_calendar, // will be changed to the logo once we make one
-                  color: Color.fromARGB(255, 58, 110, 222),
+                  color: Color.fromARGB(255, 70, 52, 237),
                   size: 150,
                 ),
               ),
-              Text(
-                'Variny Project Planner',
-                style: TextStyle(fontSize: 30),
+               Padding(
+                padding: const EdgeInsets.only(bottom: 50.0),
+                child: Text(
+                  'Variny Project Planner',
+                  style: TextStyle(fontSize: 30),
               ),
+               ),
               SizedBox(height: 20), // Add spacing between the text and the row
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ElevatedButton(
                     onPressed: () {
-                      // Add functionality for the first button here
+                      Navigator.push(
+                      context, 
+                      MaterialPageRoute(builder: (context) => const LoginPage()),
+                    );
                     },
                     child: Text('Login'),
                   ),
-                  SizedBox(width: 20), // Add spacing between the buttons
+                  SizedBox(width: 40), // Add spacing between the buttons
                   ElevatedButton(
                     onPressed: () {
-                      // Add functionality for the second button here
+                       Navigator.push(
+                        context, 
+                        MaterialPageRoute(builder: (context) => const CreateAccountPage()),
+                    );
                     },
                     child: Text('Create Account'),
                   ),
@@ -55,6 +65,37 @@ class MainApp extends StatelessWidget {
             ],
           ),
         ),
+      );
+      }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login to your account'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Second Route!'), //switch to username and password fields
+      ),
+    );
+  }
+}
+
+class CreateAccountPage extends StatelessWidget {
+  const CreateAccountPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create an Account'),
+      ),
+      body: Center(
+        child: Text('To create an account, we ask you to take a short quiz about your work/learning styles so we can personalize your schedule. To start the quiz, click the button below'), //switch to quiz 
       ),
     );
   }
